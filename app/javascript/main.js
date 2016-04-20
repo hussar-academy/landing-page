e94887325c84cc5d2606dc71ada64ed98732e87f
kamnog <kamil.nogala@gmail.com>
2016-04-20 11:43:12 +0200

    [TOPBAR] Modyfikacja istniejacego topbara + dodanie malej responsywnosci + obsluga menu (otwarte / zamkniete) via JS

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+var mobileClose = document.getElementById("mobileClose");
+var mobileOpen = document.getElementById("mobileOpen");
+var mobileMenu = document.getElementById("mobileMenu");
+var isMobileMenuOpen = false;
+
+mobileClose.onclick = function() {
+  mobileMenu.classList.remove("mobile-menu--open");
+};
+
+mobileOpen.onclick = function() {
+  mobileMenu.classList.add("mobile-menu--open");
+};
